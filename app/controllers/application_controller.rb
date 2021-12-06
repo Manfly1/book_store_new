7eaab1d96c9f278a4e6fbd2dc08e6a4d87a91f41
Manfly1 <aivanuyk0@gmail.com>
2021-12-06 18:28:25 +0200

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  include Application
+
+  private
+
+  def save_my_previous_url
+    previous_url = request.referer
+    session[:my_previous_url] = previous_url unless previous_url == request.url
+  end
 end
