c229b96e9d9ad8c14c889bcdc09dd37b84f7eba3
Manfly1 <aivanuyk0@gmail.com>
2021-12-10 14:11:58 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'home#index'
+
   resources :books
+  resources :users, only: %i[update edit destroy show]
+
+  resources :books, only: :show do
+    resources :reviews, only: :create
+  end
+
+  resources :orders do
+    resources :order_items
+  end
+
+  resource :fast_registration, only: %i[new create]
+
+  resources :checkouts, only: %i[show update]
 end
