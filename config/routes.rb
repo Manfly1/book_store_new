6ff7a4214262945925e7dc515dd04201f3b8f2d3
Manfly1 <aivanuyk0@gmail.com>
2021-12-10 14:10:26 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'home#index'
+
   resources :books
+  resources :users, only: %i[update edit destroy show]
+
+  resources :books, only: :show do
+    resources :reviews, only: :create
+  end
+
+  resources :orders do
+    resources :order_items
+  end
 end
