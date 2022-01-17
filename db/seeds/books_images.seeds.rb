49435ce1ad59e6cc74e3ad6c6e7b05a9bce9e24a
Manfly1 <aivanuyk0@gmail.com>
2022-01-17 19:10:25 +0200

    try without seeds for images

--- db/seeds/books_images.seeds.rb
+++ db/seeds/books_images.seeds.rb
@@ -3,15 +3,15 @@ start_time = 0
 after :books do
   start_time = Time.now
 
-  Book.all.each do |book|
-    rand(3..4).times do
-      book.images.attach(
-        io: open("app/assets/images/#{rand(1..24)}.jpg"),
-        filename: 'cover.jpg',
-        content_type: 'image/jpg'
-      )
-    end
-  end
+  # Book.all.each do |book|
+  #   rand(3..4).times do
+  #     book.images.attach(
+  #       io: open("app/assets/images/#{rand(1..24)}.jpg"),
+  #       filename: 'cover.jpg',
+  #       content_type: 'image/jpg'
+  #     )
+  #   end
+  # end
 end
 finish_time = Time.now
 
